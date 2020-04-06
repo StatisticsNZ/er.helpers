21bde41e80dfd970da05083f41eae388dcb82fe6
Fernando Cagua <fernando.cagua@stats.govt.nz>
2020-04-06 15:42:34 +1200

    Add some tests to anomaly calculation

--- tests/testthat/test-anomaly.R
+++ tests/testthat/test-anomaly.R
@@ -1,6 +1,31 @@
 test_that("anomaly works", {
 
-  expect_length(calc_annual_anomaly(runif(100, 10, 20),
-                                    1901:2000,
-                                    c(1961, 1990)), 100)
+  temp <- runif(100, 10, 20)
+  years <- 1901:2000
+  ref_period <- c(1961, 1990)
+
+  # output must be same length as input
+  expect_length(calc_annual_anomaly(temp, years, ref_period), 100)
+
+  # If proportion of NAs during reference period is larger than 0.2 return NA
+  temp_na <- temp
+  temp_na[60:80] <- NA
+  expect_warning({
+    calc_annual_anomaly(temp_na, years, ref_period, max_missing = 0.2) %>%
+      is.na() %>%
+      all() %>%
+      expect_equal(TRUE)
+  })
+
+  # If proportion of NA during referebce period is smaller than 0.2 then
+  # calculate the anomaly
+  temp_na <- temp
+  temp_na[61:65] <- NA
+  expect_warning({
+    calc_annual_anomaly(temp_na, years, ref_period, max_missing = 0.2) %>%
+      is.na() %>%
+      sum() %>%
+      expect_equal(5)
+  })
+
 })
